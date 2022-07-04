38713422c8a027b79f127d44b37e0f7361b82daf
AnushaBattu <51118859+AnushaBattu@users.noreply.github.com>
2022-07-04 19:39:07 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("what is your name? ");
+            var name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
